85c16bbb082017fea1deccdb4061a5baf3ad66b1
kimseongjae <stwin755@gmail.com>
2024-02-22 12:41:49 +0900

    JSON으로 CSR 구현

--- src/main/java/shop/mtcoding/blog/board/BoardApiController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardApiController.java
@@ -3,7 +3,9 @@ package shop.mtcoding.blog.board;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,6 +14,19 @@ import java.util.List;
 @RestController
 public class BoardApiController {
     private final BoardRepository boardRepository;
+
+    @DeleteMapping("/api/boards/{id}")
+    public ApiUtil<?> deleteById(@PathVariable Integer id, HttpServletResponse response) {
+        Board board = boardRepository.selectOne(id);
+        if (board == null) {
+            response.setStatus(404);
+            return new ApiUtil<>(404, "해당 게시글을 찾을 수 없습니다.");
+        }
+
+        boardRepository.deleteById(id);
+        return new ApiUtil<>(null);
+    }
+
 
     @GetMapping("/api/boards")
     public ApiUtil<?> findAll(HttpServletResponse response) {
