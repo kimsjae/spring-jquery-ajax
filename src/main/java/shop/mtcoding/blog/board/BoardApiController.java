0bfe480a16fd72b6410334dfe60105870ba65ad8
kimseongjae <stwin755@gmail.com>
2024-02-22 15:20:18 +0900

    게시글 쓰기 CSR로 구현

--- src/main/java/shop/mtcoding/blog/board/BoardApiController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardApiController.java
@@ -3,10 +3,7 @@ package shop.mtcoding.blog.board;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,6 +11,13 @@ import java.util.List;
 @RestController
 public class BoardApiController {
     private final BoardRepository boardRepository;
+
+    @PostMapping("/api/boards")
+    public ApiUtil<?> write(@RequestBody BoardRequest.WriteDTO requestDTO) { // JSON을 받을 수 있음
+        boardRepository.insert(requestDTO);
+        return new ApiUtil<>(null);
+    }
+
 
     @DeleteMapping("/api/boards/{id}")
     public ApiUtil<?> deleteById(@PathVariable Integer id, HttpServletResponse response) {
